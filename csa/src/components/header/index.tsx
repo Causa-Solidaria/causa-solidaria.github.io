a3b874a7fad4a9080bff700a906a708de385e087
Davi dos Santos Passos <fenandosouzamelo419@gmail.com>
2025-04-06 21:53:42 -0300

    Update index.tsx

--- csa/src/components/header/index.tsx
+++ csa/src/components/header/index.tsx
@@ -1,7 +1,7 @@
 //import React from "react";
 import Image from "next/image";
-import { Container, Logo} from "./styles";
 import Link from "next/link";
+import { Container, Logo} from "./styles";
 
 const Header = () => {
     return (
