ea6f030206a39a1ea5c0e008599e7e0fc218f6fd
Davi dos Santos Passos <69681778+pessoa736@users.noreply.github.com>
2025-06-16 15:34:00 +0000

    organização do path para deploy

--- csa/next.config.ts
+++ csa/next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from "next";
 
 
 const isGithubPages = process.env.GITHUB_ACTIONS || false;
-
+const repo = isGithubPages ? '/causa-solidaria-js' : ''
 
 const nextConfig: NextConfig = {
+  basePath: repo,
+  assetPrefix: repo,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: repo
+  },
   output: (isGithubPages ?  "export" : undefined),
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
