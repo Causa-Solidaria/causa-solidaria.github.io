77907d609ee4eda3366ec5f7e8ec315d6d287c9f
Davi dos Santos Passos <fenandosouzamelo419@gmail.com>
2025-10-17 14:52:26 -0300

    feat: Update Header component to manage user login state and add navigation links; create empty Nav component

--- csa/src/components/DefaultPage/Header/index.tsx
+++ csa/src/components/DefaultPage/Header/index.tsx
@@ -1,24 +1,26 @@
-import { Box, Heading } from "@chakra-ui/react"
+import { Box, Flex, Heading } from "@chakra-ui/react"
 import { staticPosition } from "csa/utils/staticPosition"
 import Logo from "csa/components/logo"
-import { ensureLogged } from "csa/utils/isloged";
+import { getToken } from "csa/utils/isloged";
 
 
 
 // O componente Main do Header
 
 const Header = () => {
-  const isLogged = ensureLogged();
+  const isLogged = getToken();
 
   return (
-    <Box 
-      display="flex" 
-      flexDirection={"row"} 
+    <Flex 
+      direction={"row"} 
       alignItems="center"
+      justifyContent={"space-between"}
       borderRadius={0} 
       position={"sticky"}
-      bg="sec"  
-      w={"100vmax"} 
+
+      bg="#00B944"  
+      maxW={"100vmax"}
+      minW={"100vmax"}
       minH={staticPosition(244, 3197)} 
       h={staticPosition(244, 3197)} 
       left={0} 
@@ -31,7 +33,6 @@ const Header = () => {
         display={"flex"}
         flexDirection={"row"}
         alignItems={"center"}
-        justifyContent={"center"}
         justifyItems={"center"}
         textAlign={"center"}
         p={staticPosition(10, 3197)}
@@ -58,10 +59,49 @@ const Header = () => {
         >
           causa solidaria
         </Heading>
-
-        {isLogged ? <Box></Box> : <Box></Box>}
       </Box>
-    </Box>
+      
+      <Flex
+        direction={"row"}
+        alignItems={"center"}
+        gap={staticPosition(40, 3197)}
+      >
+        {!isLogged && <Flex
+          direction={"row"}
+          gap={staticPosition(40, 3197)}
+        >
+          {[
+            {label: "entrar", link: "/login"},
+            {label: "cadastrar", link: "/cadastro"}
+          ].map(({label, link}, index) => (
+            <Box
+              key={index}
+              onClick={()=>{window.location.href = link}}
+              maxH={staticPosition(70, 3197)}
+              minH={staticPosition(70, 3197)}
+              bg = {"#006E1F"}
+              px={staticPosition(30, 3197)}
+              borderRadius={staticPosition(12, 3197)}
+              color={"#fff"}
+              fontWeight={"900"}
+            >
+              {label}
+            </Box>
+          ))}
+        </Flex>}
+
+        <Box 
+          maxW={staticPosition(137, 3197)}
+          minW={staticPosition(137, 3197)}
+          maxH={staticPosition(137, 3197)}
+          minH={staticPosition(137, 3197)}
+
+          border={`${staticPosition(3, 3197)} solid #000`}
+        >
+
+        </Box>
+      </Flex>
+    </Flex>
   )
 }
 
