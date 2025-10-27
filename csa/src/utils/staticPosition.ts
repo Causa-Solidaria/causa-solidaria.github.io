ed81f7037e066bdf3a29b46fbafe68fcf402c166
Davi <69681778+pessoa736@users.noreply.github.com>
2025-10-27 14:12:06 -0300

    update: agora da criar listas de static position

--- csa/src/utils/staticPosition.ts
+++ csa/src/utils/staticPosition.ts
@@ -1,6 +1,16 @@
 
 
 
-export function staticPosition(size:number | string, maxsize:number | string = 1300) {
-    return `calc(100vmax * ${size} / ${maxsize})`
+export function staticPosition(size:number | string | (number | string)[], maxsize:number | string = 1300) {
+    let result: string | string[]
+    if (Array.isArray(size)){
+        const arr = (size as (number | string)[]).map(
+            value => `calc(100vmax * ${value} / ${maxsize})`
+        )
+        result = arr
+    }else{
+        result = `calc(100vmax * ${size} / ${maxsize})`
+    }
+
+    return result
 }
