2b78c4d03270d277dc6cc3b0e7ce24b19b33c6bd
Davi <69681778+pessoa736@users.noreply.github.com>
2025-12-17 13:40:02 -0300

    melhorando o sistema do butão para ter modelos do mesmo

--- csa/src/components/ui/Button.tsx
+++ csa/src/components/ui/Button.tsx
@@ -1,6 +1,28 @@
 import { Button as B, ButtonProps } from "@chakra-ui/react"
+import { staticPosition } from "csa/lib/utils"
 import React from 'react'
 
-export default function Button(props: ButtonProps){
-    return <B {...props}>{props.children}</B>
+interface Bprops extends ButtonProps {
+    level?: (1 | 2 | 3) & number 
+}
+
+
+const levels: ButtonProps[] = []
+levels[1] = {bg: "#097D03", color: "#fff"}
+levels[2] = {}
+levels[3] = {}
+
+
+export default function Button(props: Bprops){
+    const Blevel  = levels[props.level as number || 1]
+
+    return (
+    <B
+        m={"1vmax"}
+        p={"1vmax"}
+        borderRadius={staticPosition(10, 1280)}
+        justifyContent={"center"}
+        {...(Blevel || {})}
+        {...props}
+    >{props.children}</B>)
 }
