35b12694b0d6eb47c8db3a268f3a1a807d062250
Davi <69681778+pessoa736@users.noreply.github.com>
2026-01-18 13:22:46 -0300

    remove: retirado o import desnecessario do next.js na home

--- csa/src/pages/index.tsx
+++ csa/src/pages/index.tsx
@@ -3,7 +3,6 @@
 import DefaultPage from "csa/components/DefaultPage"
 import CarrosselOngs from "csa/components/pagesComponents/CarrosselOngs"
 import { HeroSection, AboutSection } from "csa/components/pagesComponents/Home"
-import type { NextPage } from "next"
 
 export default function Home() {
   return (
